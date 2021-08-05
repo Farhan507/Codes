76ba48a64649159b337e12ca3f4edd412211cabd
Farhan507 <54713211+Farhan507@users.noreply.github.com>
2021-08-05 16:22:53 +0530

    Create Q3_MoFarhanRajkotiya.py

---
+++ Q3_MoFarhanRajkotiya.py
@@ -0,0 +1,17 @@
+def valleyCount(steps, path):
+
+    level = 0
+    valley = 0
+    
+    for step in path:
+    
+        if step == 'U':
+            level+=1
+            
+            if level == 0:
+                valley+=1
+                
+        else:
+            level-=1
+            
+    return valley
